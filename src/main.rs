bc8101e1fc390ee8c9fae8dfadfd3fb00bd8539e
Thorinori <thorinori@gmail.com>
2024-05-28 03:08:27 -0600

    Initial Speech Recognition + Processing

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,14 @@ fn main() {
     //Set up Mic Input
     let host = cpal::default_host();
     let input_device = host.default_input_device().expect("Missing Input Device");
-    println!("Input Device: {}", input_device.name().unwrap());
+
     let mut supported_configs = input_device.supported_input_configs().expect("Error Querying Device Configs");
     let supported_config = supported_configs.next().expect("No Supported Configs!").with_max_sample_rate();
+
     let sample_rate = supported_config.sample_rate().0;
     let channels = supported_config.channels();
     let sample_format = supported_config.sample_format();
+
     let config = supported_config.into();
     let err_fn = |err| eprint!("Error Occured on the Input Audio Stream: {}", err);
 
@@ -50,27 +52,28 @@ fn main() {
                 "eight", "nine", "ten", "zero", "negative", "positive", "hundred", "thousand",
                 "million", "billion", "trillion", "page", "home", "insert", "delete", "end",
                 "scroll", "lock", "print", "screen", "pause", "num", "dollar", "shift", "return",
-                "escape", "quit", "exit", "begin", "stop", "input"
+                "escape", "quit", "exit", "begin", "stop",
     ];
     for word in words{
         if let None = model.find_word(word) {
             println!("Model couldn't find word: {}", word);
         }
     }
- 
-   //Create input stream 
+
+    //Create input stream 
     let stream = match sample_format{
-        SampleFormat::F32 => input_device.build_input_stream(&config, move |data: &[f32], _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels),err_fn , None),
-        SampleFormat::I16 => input_device.build_input_stream(&config, move |data: &[i16], _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels),err_fn , None),
-        SampleFormat::U16 => input_device.build_input_stream(&config, move |data: &[u16], _| process_input(&mut recognizer.lock().unwrap(), data, channels),err_fn , None),
-        SampleFormat::U8  => input_device.build_input_stream(&config, move |data: &[u8], _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels), err_fn , None),
-        sample_format => panic!("Unsupported Sample Format '{sample_format}'")
+       SampleFormat::F32 => input_device.build_input_stream(&config, move |data: &[f32], _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels),err_fn , None),
+       SampleFormat::I16 => input_device.build_input_stream(&config, move |data: &[i16], _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels),err_fn , None),
+       SampleFormat::U16 => input_device.build_input_stream(&config, move |data: &[u16], _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels),err_fn , None),
+       SampleFormat::U8  => input_device.build_input_stream(&config, move |data: &[u8],  _| process_input(&mut recognizer_clone.lock().unwrap(), data, channels),err_fn , None),
+       sample_format => panic!("Unsupported Sample Format '{sample_format}'")
     }.unwrap();
-   
-   
+
 
     stream.play().unwrap();
-    // std::thread::sleep(Duration::from_secs(2));
+    loop{
+        std::thread::sleep(Duration::from_millis(500));
+    }     
 }
 
 fn process_input<T: Sample + ToSample<i16>>(recognizer: &mut Recognizer, data: &[T], channels: ChannelCount){
@@ -85,7 +88,14 @@ fn process_input<T: Sample + ToSample<i16>>(recognizer: &mut Recognizer, data: &
     match state {
         vosk::DecodingState::Running => {}
         vosk::DecodingState::Finalized => {
-            print!("Word Recognized: {:#?}", recognizer.result().multiple().unwrap())
+            let res = recognizer.result().multiple().unwrap();
+            let text = res.alternatives[0].text;
+            if text.eq("quit") || text.eq("exit"){
+                println!("Exiting Vimcal...");   
+                std::process::exit(0);
+            }else{
+                println!("Word was: {}", text);
+            }
         }
         vosk::DecodingState::Failed => eprintln!("Recognizer Errored"),
     }
